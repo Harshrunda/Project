cc39310634da9ac2b49bdc860cdb9972ca904983
Harshrunda <harshrunda48@gmail.com>
2024-07-20 20:57:34 +0530

    new

---
+++ program.py
@@ -0,0 +1,14 @@
+name =input("enter your name:")
+age = int(input("enter your age"))
+a=float(input("enter a number"))
+b=float(input("another number"))
+print("my name is :",name ,"my age is:",age)
+sum = a+b
+product = a*b
+remainder =a%b
+c=a**b
+print ("sum =",sum)
+print ("product =",product)
+print ("Remainder =",remainder)
+print ("power=",c)
+print(product>=remainder )
